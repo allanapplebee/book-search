6614834dfee038ac42104a1daea383ccd1b0811b
Allan Applebee <allan.applebee@gmail.com>
2019-02-28 08:36:48 +1100

    card style added to booklist

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -7,19 +7,34 @@ const BookList = ({books}) => {
 
     else {
         return (  
-            <ul>
+            <div className="container-fluid">
+            
                 {books.map(book =>
-                <li key={book.id}>
-                    <div>
-                        <img alt="book cover" src={'imageLinks' in book.volumeInfo ? book.volumeInfo.imageLinks.thumbnail : 'https://upload.wikimedia.org/wikipedia/en/f/f9/No-image-available.jpg'} />
-                        <p>{book.volumeInfo.title}</p>
-                        <p>By: {'authors' in book.volumeInfo ? book.volumeInfo.authors : 'Unknown'}</p>
-                        <p>Published by: {'publisher' in book.volumeInfo ? book.volumeInfo.publisher : 'Unknown'}</p>
-                        <a className="btn btn-primary" href={book.volumeInfo.previewLink}>See this book</a>
+                // <li key={book.id}>
+                    <div key={book.id} class="row">
+                        <div className="col-sm-6">
+                            <div className="card">
+                            <img className="card-img-top" alt="book cover" src={'imageLinks' in book.volumeInfo ? book.volumeInfo.imageLinks.thumbnail : 'https://upload.wikimedia.org/wikipedia/en/f/f9/No-image-available.jpg'} />
+                            <div className="card-body">
+                                <h5 className="card-title">{book.volumeInfo.title}</h5>
+                                <p className="card-text">By: {'authors' in book.volumeInfo ? book.volumeInfo.authors : 'Unknown'}</p>
+                                <p className="card-text">Published by: {'publisher' in book.volumeInfo ? book.volumeInfo.publisher : 'Unknown'}</p>
+                                <a href="{book.volumeInfo.previewLink}" className="btn btn-primary">See this book</a>
+                            </div>
+                            </div>
+                        </div>
                     </div>
-                </li>
+            
+                    // <div>
+                        // <img alt="book cover" src={'imageLinks' in book.volumeInfo ? book.volumeInfo.imageLinks.thumbnail : 'https://upload.wikimedia.org/wikipedia/en/f/f9/No-image-available.jpg'} />
+                        // <p>{book.volumeInfo.title}</p>
+                        // <p>By: {'authors' in book.volumeInfo ? book.volumeInfo.authors : 'Unknown'}</p>
+                        // <p>Published by: {'publisher' in book.volumeInfo ? book.volumeInfo.publisher : 'Unknown'}</p>
+                        // <a className="btn btn-primary" href={book.volumeInfo.previewLink}>See this book</a>
+                    // </div>
+                    // </li>
                 )};
-            </ul>
+            </div>
         )
     }
 }
